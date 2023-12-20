9fa025d778c60919953c607bf78327168af88cf1
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2023-12-20 21:28:13 +0700

    New updates for Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k8w%xbf(vgj7vpvntg9f=rawsh#0a8f#sc@ed36s1h1&!2hccm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
